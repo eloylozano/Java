b17cb0b160a310eff8691341e992f97b0cb0a987
eloylozano <lozanobarrioseloy@gmail.com>
2023-12-04 08:58:17 +0100

    correccion

--- Arrays/untitled/src/Ejercicio06/Main.java
+++ Arrays/untitled/src/Ejercicio06/Main.java
@@ -5,29 +5,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Clase para las entradas
         Scanner teclado = new Scanner(System.in);
-
-        // Clase para el random
         Random numero = new Random();
-
-        // Declarar array de longitud 10 valores enteros
         int[] array = new int[10];
 
         // Recorremos el array asignando un número random a cada posición y se imprime
         for (int i = 0; i < array.length; i++) {
             int answer = numero.nextInt(10);
             array[i] = answer;
-            System.out.println(" · Objeto " + i + " = " + answer);
+            System.out.print(answer + " ");
         }
 
         // Creamos la entrada para pedir el número
         System.out.print("\nIntroduce un número para buscarlo en el array: ");
         int findNumber = teclado.nextInt();
 
-        // Creamos la variable para el contador
         int contador = 0;
-
         // Recorremos el array buscando el número y sumando el contador
         for (int i = 0; i < array.length; i++) {
             if (findNumber == array[i]) {
