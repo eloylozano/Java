3c0cf509a48b71f99f14f676bf5d4ebb3b3e207d
Eloy <lozanobarrioseloy@gmail.com>
2024-01-27 15:53:49 +0100

    exercise 04 progress

---
+++ OrientadoObjetos01/src/Ejercicio04/Empleado.java
@@ -0,0 +1,17 @@
+package Ejercicio04;
+
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int salario;
+
+    public Empleado(String dni, String nombre, String apellidos, int salario) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.salario = salario;
+    }
+
+
+}
---
+++ OrientadoObjetos01/src/Ejercicio04/Main.java
@@ -0,0 +1,7 @@
+package Ejercicio04;
+
+public class Main {
+    public static void main(String[] args) {
+        Empleado empleado01 = new Empleado("12789671W", "Pepe", "Rodriguez", 1200);
+    }
+}
