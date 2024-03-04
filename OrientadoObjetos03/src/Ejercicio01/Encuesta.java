1e5f3845aec5485a91c4f870b159a1c5ceca58f9
eloylozano <lozanobarrioseloy@gmail.com>
2024-03-04 22:36:08 +0100

    new exercise

---
+++ OrientadoObjetos03/src/Ejercicio01/Encuesta.java
@@ -0,0 +1,13 @@
+package Ejercicio01;
+
+public class Encuesta {
+    private String Pregunta;
+    private int VotosA;
+    private int VotosB;
+
+    public Encuesta(String pregunta, int votosA, int votosB) {
+        Pregunta = pregunta;
+        VotosA = 0;
+        VotosB = 0;
+    }
+}
