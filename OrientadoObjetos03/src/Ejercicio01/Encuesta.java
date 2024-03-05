256eff4c7230a45df1361ecf4249c7df860bee60
eloylozano <lozanobarrioseloy@gmail.com>
2024-03-05 17:52:39 +0100

    new exercise

--- OrientadoObjetos03/src/Ejercicio01/Encuesta.java
+++ OrientadoObjetos03/src/Ejercicio01/Encuesta.java
@@ -2,12 +2,42 @@ package Ejercicio01;
 
 public class Encuesta {
     private String Pregunta;
+    private String OpcionA;
+    private String OpcionB;
     private int VotosA;
     private int VotosB;
 
-    public Encuesta(String pregunta, int votosA, int votosB) {
+    public Encuesta(String pregunta, String opcionA, String opcionB, int votosA, int votosB) {
         Pregunta = pregunta;
-        VotosA = 0;
-        VotosB = 0;
+        OpcionA = opcionA;
+        OpcionB = opcionB;
+        VotosA = votosA;
+        VotosB = votosB;
     }
+
+    public int incA() {
+        return VotosA++;
+    }
+
+    public int incB() {
+        return VotosB++;
+    }
+
+    public void total_Votos() {
+        int totalVotos = VotosA + VotosB;
+        if (VotosA == 0) {
+            System.out.println("\nEl resultado de votos es: ");
+            System.out.println(" · Votos A: 0%");
+            System.out.println(" · Votos B: 100%");
+        } else if (VotosB == 0) {
+            System.out.println("\nEl resultado de votos es: ");
+            System.out.println(" · Votos A: 100%");
+            System.out.println(" · Votos B: 0%");
+        } else {
+            System.out.println("\nEl resultado de votos es: ");
+            System.out.println(" · Votos A: " + Math.round((double)VotosA / totalVotos * 100) + "%. Con un tota de: "+ VotosA + " votos.");
+            System.out.println(" · Votos B: " + Math.round((double)VotosB / totalVotos * 100) + "%. Con un tota de: "+ VotosB + " votos.");
+        }
+    }
+
 }
--- OrientadoObjetos03/src/Ejercicio01/Main.java
+++ OrientadoObjetos03/src/Ejercicio01/Main.java
@@ -1,7 +1,60 @@
 package Ejercicio01;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random randon = new Random();
+
+        System.out.print("Escribe una pregunta para crear una encuesta: ");
+        String pregunta = sc.nextLine();
+
+        System.out.print("Escribe la opción A, para votar: ");
+        String opcionA = sc.nextLine();
+
+        System.out.print("Escribe la opción B, para votar: ");
+        String opcionB = sc.nextLine();
+
+
+        System.out.println("""
+                ¿Cuánta gente votará?
+                 1. Entre 0-10
+                 2. Entre 0-100
+                 3. Entre 0-1000""");
+        System.out.print("Tu opción: ");
+
+        int opcion;
+        int genteVotando = 0;
+        opcion = sc.nextInt();
+        switch (opcion) {
+            case 1:
+                genteVotando = randon.nextInt(10);
+                break;
+            case 2:
+                genteVotando = randon.nextInt(90) + 10;
+                break;
+            case 3:
+                genteVotando = randon.nextInt(900) + 100;
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+
+        int votosParaA = 0;
+        int votosParaB = 0;
+        for (int i = 0; i < genteVotando; i++) {
+            int temp = randon.nextInt(100);
+            if (temp <= 50) {
+                votosParaA++;
+            } else if (temp > 50) {
+                votosParaB++;
+            }
+        }
+
+        Encuesta encuesta01 = new Encuesta(pregunta, opcionA, opcionB, votosParaA, votosParaB);
+        encuesta01.total_Votos();
 
     }
 }
